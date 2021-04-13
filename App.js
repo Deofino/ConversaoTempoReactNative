6843f9748ba7c65c9046bbbcc3d60d2b97080d37
gDelps <guilhermedelfino25@gmail.com>
2021-04-12 23:48:08 -0300

    Tudo OK

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import { StyleSheet, View, ActivityIndicator } from 'react-native';
-import { Text, ThemeProvider, Input, Button } from 'react-native-elements';
+import { Text, ThemeProvider, Input, } from 'react-native-elements';
 import Icon from '@expo/vector-icons/FontAwesome';
 import { StatusBar } from 'expo-status-bar';
 import { useFonts,Poppins_400Regular,Poppins_600SemiBold,Poppins_300Light } from "@expo-google-fonts/poppins";
@@ -35,14 +35,28 @@ const theme = {
 }
 
 const App = () => {
-  
+  const regex = /[0-9]/gi;
+
+  const [segundos,setSegundos] = useState(null);
+
+  const [Resto,setResto] = useState(0);
+  const [Horas,setHoras] = useState(0);
+  const [Minutos,setMinutos] = useState(0);
+  const [Error,setError] = useState('');
+
+  useEffect(()=>{
+    setHoras(parseInt(segundos/3600));
+    setMinutos(parseInt(segundos%3600/60));
+    setResto(parseInt(segundos%3600%60));
+  },[segundos])
+
   const [fonts] = useFonts({
     Poppins_400Regular,
     Poppins_600SemiBold,
     Poppins_300Light
   });
   if(!fonts){
-    return (<ActivityIndicator />);
+    return (<ActivityIndicator  />);
   }
   return (
     <ThemeProvider theme={theme}>
@@ -50,10 +64,24 @@ const App = () => {
 
         <Text h3>Conversão de tempo</Text>
         <Input label='Valor em segundos' 
+        style={{
+          alignItems: "baseline",
+        }}
           rightIcon={<Icon name='clock-o' size={25} color='#0D1113' />} 
           keyboardType='number-pad'
+          value={segundos}
+          errorStyle={styles.medium}
+          placeholder='60s = 1m | 60m = 1h'
+          errorMessage={Error}
+          onChangeText={(text)=>{
+            if(text.length>0){
+              if(!regex.test(text.charAt(text.length-1))){
+                setError('Esse campo aceita somente numeros');
+              }else {setSegundos(text);setError('')};
+            }else {setSegundos('');setError('')};
+          }}
         />
-        
+        {segundos!=null&&segundos!='' && <Text style={{fontSize: 16}}>Horas <Text style={styles.bold}>{Horas}</Text> | Minutos  <Text style={styles.bold}>{Minutos}</Text> | Segundos <Text style={styles.bold}>{Resto}</Text></Text>}
         <StatusBar style='auto' animated />
       </View>
     </ThemeProvider>
@@ -69,5 +97,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#0D1113',
+  },
+  bold:{
+    fontFamily: 'Poppins_600SemiBold',
+  },
+  medium:{
+    fontFamily: 'Poppins_400Regular',
   }
 })
